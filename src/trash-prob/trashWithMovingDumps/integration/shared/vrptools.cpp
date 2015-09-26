b2ed0174d90013a6e7ad434da1b59874cba3ed69
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2015-09-26 11:23:42 -0300

    Se agrega esqueleto para biblioteca de enlace dinamico

---
+++ src/trash-prob/trashWithMovingDumps/integration/shared/vrptools.cpp
@@ -0,0 +1,9 @@
+#include "vrptools.h"
+
+void VRPTools::setx(int newx) { 
+    myx = newx; 
+}
+
+int  VRPTools::getx() { 
+    return myx; 
+}
---
+++ src/trash-prob/trashWithMovingDumps/integration/shared/vrptools.h
@@ -0,0 +1,9 @@
+class VRPTools {
+   int myx;
+
+  public:
+
+    VRPTools() { myx=0; }
+    void setx (int newx);
+    int  getx();
+};
