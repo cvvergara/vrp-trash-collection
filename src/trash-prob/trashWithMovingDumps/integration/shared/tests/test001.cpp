59600389ac28c524acd667773daebb33d2fdd94e
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2015-09-28 17:06:51 -0300

    Se agregan tests para pruebas

---
+++ src/trash-prob/trashWithMovingDumps/integration/shared/tests/test001.cpp
@@ -0,0 +1,15 @@
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <math.h>
+#include <stdio.h>
+
+#include "vrptools.h"
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
